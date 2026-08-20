3ba64ec3ee5ba537b9dcdd2e8be40954fa5a1d58
facchinn <nazafacchin@hotmail.com>
2026-08-20 15:23:37 -0300

    use token extractor middleware

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 
 const app = express()
 
@@ -25,6 +26,7 @@ mongoose
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.tokenExtractor)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
