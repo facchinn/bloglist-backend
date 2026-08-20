963dae6df46551a82f47f8eacd2712290ccac3dd
facchinn <nazafacchin@hotmail.com>
2026-08-20 16:08:41 -0300

    complete exercise 4.22 user extractor

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
 const tokenExtractor = (request, response, next) => {
   const authorization = request.get('authorization')
 
@@ -10,6 +13,29 @@ const tokenExtractor = (request, response, next) => {
   next()
 }
 
+const userExtractor = async (request, response, next) => {
+  if (!request.token) {
+    request.user = null
+    return next()
+  }
+
+  try {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+
+    if (!decodedToken.id) {
+      request.user = null
+      return next()
+    }
+
+    request.user = await User.findById(decodedToken.id)
+    next()
+  } catch (error) {
+    request.user = null
+    next()
+  }
+}
+
 module.exports = {
   tokenExtractor,
+  userExtractor,
 }
