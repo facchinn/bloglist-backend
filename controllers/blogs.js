c9d291566297f1152b74a4fa56f1a962207cafc8
facchinn <nazafacchin@hotmail.com>
2026-08-20 14:42:31 -0300

    complete exercise 4.17 blog creator

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,8 +1,9 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
@@ -13,14 +14,24 @@ blogsRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: 'title and url are required' })
   }
 
+  const user = await User.findOne({})
+
+  if (!user) {
+    return response.status(400).json({ error: 'no users available' })
+  }
+
   const blog = new Blog({
     title,
     author,
     url,
     likes,
+    user: user._id,
   })
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   response.status(201).json(savedBlog)
 })
 
