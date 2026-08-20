15ce2b5b18bd1a525d0558df0ea0f331ea8541a4
Nazareno <nazaf@Nazareno.(none)>
2026-08-20 13:54:16 -0300

    estructura

---
+++ app.js
@@ -0,0 +1,29 @@
+const config = require('./utils/config')
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+
+const app = express()
+
+mongoose.set('strictQuery', false)
+
+logger.info('connecting to MongoDB')
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
